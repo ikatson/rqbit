2b9129221db34db7758154ba368ffdc4fbd548f7
Igor Katson <igor.katson@gmail.com>
2023-12-07 22:51:31 +0000

    Add vite react plugin

--- crates/librqbit/webui/vite.config.ts
+++ crates/librqbit/webui/vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	//	plugins: [react()],
-	server: {
-		port: 3031
-	},
-	build: {
-		manifest: true
-	}
-
+  plugins: [react()],
+  server: {
+    port: 3031,
+  },
+  build: {
+    manifest: true,
+  },
 });
