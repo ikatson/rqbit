0ff63d8a245a2553984883723720fbc215fc4c69
Igor Katson <igor.katson@gmail.com>
2024-01-02 18:43:25 +0000

    Fix progress bar color when checking files

--- crates/librqbit/webui/src/components/ProgressBar.tsx
+++ crates/librqbit/webui/src/components/ProgressBar.tsx
@@ -4,20 +4,23 @@ type Props = {
   variant?: "warn" | "info" | "success" | "error";
 };
 
+const variantClassNames = {
+  warn: "bg-amber-500 text-white",
+  info: "bg-blue-500 text-white",
+  success: "bg-green-700 text-white",
+  error: "bg-red-500 text-white",
+};
+
 export const ProgressBar = ({ now, variant, label }: Props) => {
   const progressLabel = label ?? `${now.toFixed(2)}%`;
 
-  const variantClassName = {
-    warn: "bg-amber-500",
-    info: "bg-blue-500 text-white",
-    success: "bg-green-700 text-white",
-    error: "bg-red-500 text-white",
-  }[variant ?? "info"];
+  const variantClassName =
+    variantClassNames[variant ?? "info"] ?? variantClassNames["info"];
 
   return (
     <div className={"w-full bg-gray-200 rounded-full dark:bg-gray-500"}>
       <div
-        className={`text-xs bg-blue-500 text-white font-medium transition-all text-center p-0.5 leading-none rounded-full ${variantClassName}`}
+        className={`text-xs font-medium transition-all text-center p-0.5 leading-none rounded-full ${variantClassName}`}
         style={{ width: `${now}%` }}
       >
         {progressLabel}
