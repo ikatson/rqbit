3942483f252aa03cc4a557c5321dd6b5afd9acea
Igor Katson <igor.katson@gmail.com>
2024-03-29 12:34:13 +0000

    Fix a bug after latest PR

--- crates/buffers/src/lib.rs
+++ crates/buffers/src/lib.rs
@@ -8,6 +8,7 @@ use serde::Deserialize;
 use clone_to_owned::CloneToOwned;
 
 #[derive(Default, Deserialize, PartialEq, Eq, Hash, Clone, PartialOrd, Ord)]
+#[serde(transparent)]
 pub struct ByteBufOwned(pub Box<[u8]>);
 
 #[derive(Default, Deserialize, PartialEq, Eq, Hash, Clone, PartialOrd, Ord)]
