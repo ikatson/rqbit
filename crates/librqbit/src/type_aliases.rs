40c00a778b3c74effb6013806884443c63fcf31b
Igor Katson <igor.katson@gmail.com>
2024-04-05 15:38:21 +0400

    Trying to fix rust 1.73

--- crates/librqbit/src/type_aliases.rs
+++ crates/librqbit/src/type_aliases.rs
@@ -8,4 +8,4 @@ pub type BF = bitvec::boxed::BitBox<u8, bitvec::order::Msb0>;
 
 pub type PeerHandle = SocketAddr;
 pub type PeerStream = BoxStream<'static, SocketAddr>;
-pub type OpenedFiles = Vec<OpenedFile>;
+pub(crate) type OpenedFiles = Vec<OpenedFile>;
