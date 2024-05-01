371d1ff927a472478d58fe7736e7337e35f49ee9
Igor Katson <igor.katson@gmail.com>
2024-05-01 10:47:03 +0100

    Bump up peer semaphore

--- crates/librqbit/src/torrent_state/live/mod.rs
+++ crates/librqbit/src/torrent_state/live/mod.rs
@@ -1277,7 +1277,7 @@ impl PeerHandler {
         trace!("we are unchoked");
         self.locked.write().i_am_choked = false;
         self.unchoke_notify.notify_waiters();
-        self.requests_sem.add_permits(16);
+        self.requests_sem.add_permits(64);
     }
 
     fn on_received_piece(&self, piece: Piece<ByteBuf>) -> anyhow::Result<()> {
