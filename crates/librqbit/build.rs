14aa4a2f1168e979485a529b2e29e00550642a6b
Igor Katson <igor.katson@gmail.com>
2024-08-12 20:07:47 +0100

    generate webui dist files as part of build.rs

---
+++ crates/librqbit/build.rs
@@ -0,0 +1,27 @@
+use std::path::Path;
+use std::process::Command;
+
+fn main() {
+    let webui_dir = Path::new("webui");
+    let webui_src_dir = webui_dir.join("src");
+
+    println!("cargo:rerun-if-changed={}", webui_src_dir.to_str().unwrap());
+
+    // Run "npm run build" in the webui directory
+    let output = Command::new("npm")
+        .arg("run")
+        .arg("build")
+        .current_dir(webui_dir)
+        .output()
+        .expect("Failed to execute npm run build");
+
+    if !output.status.success() {
+        panic!(
+            "npm run build failed with output: {}",
+            String::from_utf8_lossy(&output.stderr)
+        );
+    }
+
+    // Optionally print the stdout output if you want to see the build logs
+    println!("{}", String::from_utf8_lossy(&output.stdout));
+}
