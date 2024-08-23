6a02ff6e256a87f7e466dacb1de03331aa2df482
Igor Katson <igor.katson@gmail.com>
2024-08-23 01:06:15 +0100

    Saving

--- crates/librqbit/src/upnp_server_adapter.rs
+++ crates/librqbit/src/upnp_server_adapter.rs
@@ -10,44 +10,52 @@ pub struct UpnpServerSessionAdapter {
 }
 
 use anyhow::Context;
+use tracing::warn;
 use upnp_serve::{
-    upnp_types::content_directory::{response::ItemOrContainer, ContentDirectoryBrowseProvider},
+    upnp_types::content_directory::{
+        response::{Item, ItemOrContainer},
+        ContentDirectoryBrowseProvider,
+    },
     UpnpServer, UpnpServerOptions,
 };
 
 impl ContentDirectoryBrowseProvider for UpnpServerSessionAdapter {
     fn browse_direct_children(&self, parent_id: usize) -> Vec<ItemOrContainer> {
-        todo!()
-    }
+        if parent_id != 0 {
+            warn!(parent_id, "UPNP request for parent_id != 0, not supported");
+            return vec![];
+        }
 
-    // fn browse(&self) -> Vec<upnp_serve::ContentDirectoryBrowseItem> {
-    //     let hostname = &self.hostname;
-    //     let port = self.port;
-    //     self.session.with_torrents(|torrents| {
-    //         torrents
-    //             .flat_map(|(id, t)| {
-    //                 t.shared()
-    //                     .file_infos
-    //                     .iter()
-    //                     .enumerate()
-    //                     .map(move |(fid, fi)| {
-    //                         let mime_type = mime_guess::from_path(&fi.relative_filename)
-    //                             .first()
-    //                             .map(|m| m.to_string());
-    //                         let title = fi.relative_filename.to_string_lossy();
-    //                         let url = format!(
-    //                             "http://{hostname}:{port}/torrents/{id}/stream/{fid}/{title}"
-    //                         );
-    //                         ContentDirectoryBrowseItem {
-    //                             title: title.into_owned(),
-    //                             mime_type,
-    //                             url,
-    //                         }
-    //                     })
-    //             })
-    //             .collect()
-    //     })
-    // }
+        let hostname = &self.hostname;
+        let port = self.port;
+        let mut next_id = 0;
+        self.session.with_torrents(|torrents| {
+            torrents
+                .flat_map(|(id, t)| {
+                    t.shared()
+                        .file_infos
+                        .iter()
+                        .enumerate()
+                        .filter_map(move |(fid, fi)| {
+                            let mime_type = mime_guess::from_path(&fi.relative_filename).first();
+                            let title = fi.relative_filename.file_stem()?.to_string_lossy();
+                            let url = format!(
+                                "http://{hostname}:{port}/torrents/{id}/stream/{fid}/{title}"
+                            );
+                            let fake_id = next_id;
+                            next_id += 1;
+                            Some(ItemOrContainer::Item(Item {
+                                title: title.into_owned(),
+                                mime_type,
+                                url,
+                                id: fake_id,
+                                parent_id: None,
+                            }))
+                        })
+                })
+                .collect()
+        })
+    }
 }
 
 impl Session {
