22e06c1bb450a3e92f5a627a509d04656d41de57
Igor Katson <igor.katson@gmail.com>
2025-02-06 14:12:35 +0000

    dynamic e2e addres

--- crates/librqbit/src/tests/e2e_another_local_client.rs
+++ crates/librqbit/src/tests/e2e_another_local_client.rs
@@ -22,45 +22,61 @@ const OUTPUT_FOLDER: &str = "/tmp/utptest";
 // here for easy profiling.
 const BINARY_SYMLINK: &str = "/tmp/rtest";
 
-const LISTEN_ADDR: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 57318);
+const DEFAULT_LISTEN_ADDR: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 57318);
 
 // Serve the above file from the other client.
-const OTHER_CLIENT_ADDR: SocketAddr =
+const DEFAULT_OTHER_CLIENT_ADDR: SocketAddr =
     SocketAddr::new(std::net::IpAddr::V4(Ipv4Addr::LOCALHOST), 27312);
+
+fn parse_listen_and_client_addr() -> (SocketAddr, SocketAddr) {
+    let listen =
+        std::env::var("E2E_LISTEN_ADDR").map_or(DEFAULT_LISTEN_ADDR, |v| v.parse().unwrap());
+    let client =
+        std::env::var("E2E_CLIENT_ADDR").map_or(DEFAULT_OTHER_CLIENT_ADDR, |v| v.parse().unwrap());
+    (listen, client)
+}
 
 #[tokio::test(flavor = "multi_thread")]
 #[ignore]
 async fn test_utp_with_another_client() {
-    test_with_another_client(crate::SessionOptions {
-        disable_dht: true,
-        persistence: None,
-        listen: Some(ListenerOptions {
-            mode: crate::listen::ListenerMode::UtpOnly,
-            listen_addr: LISTEN_ADDR,
+    let (listen_addr, client_addr) = parse_listen_and_client_addr();
+    test_with_another_client(
+        crate::SessionOptions {
+            disable_dht: true,
+            persistence: None,
+            listen: Some(ListenerOptions {
+                mode: crate::listen::ListenerMode::UtpOnly,
+                listen_addr,
+                ..Default::default()
+            }),
+            connect: Some(ConnectionOptions {
+                enable_tcp: false,
+                ..Default::default()
+            }),
             ..Default::default()
-        }),
-        connect: Some(ConnectionOptions {
-            enable_tcp: false,
-            ..Default::default()
-        }),
-        ..Default::default()
-    })
+        },
+        client_addr,
+    )
     .await
 }
 
 #[tokio::test(flavor = "multi_thread")]
 #[ignore]
 async fn test_tcp_with_another_client() {
-    test_with_another_client(crate::SessionOptions {
-        disable_dht: true,
-        persistence: None,
-        listen: Some(ListenerOptions {
-            mode: crate::listen::ListenerMode::TcpOnly,
-            listen_addr: (Ipv4Addr::LOCALHOST, 57318).into(),
+    let (listen_addr, client_addr) = parse_listen_and_client_addr();
+    test_with_another_client(
+        crate::SessionOptions {
+            disable_dht: true,
+            persistence: None,
+            listen: Some(ListenerOptions {
+                mode: crate::listen::ListenerMode::TcpOnly,
+                listen_addr,
+                ..Default::default()
+            }),
             ..Default::default()
-        }),
-        ..Default::default()
-    })
+        },
+        client_addr,
+    )
     .await
 }
 
@@ -69,7 +85,7 @@ async fn test_tcp_with_another_client() {
 //
 // The canary file is created from librqbit_utp, then served from the other torrent client:
 // cargo run --release --example create_canary_file /tmp/canary_4096m 4096
-async fn test_with_another_client(sopts: SessionOptions) {
+async fn test_with_another_client(sopts: SessionOptions, addr: SocketAddr) {
     let test_filename = {
         let f = PathBuf::from(std::env::args().next().unwrap());
         f.read_link().unwrap_or(f)
@@ -95,7 +111,7 @@ async fn test_with_another_client(sopts: SessionOptions) {
             crate::AddTorrent::TorrentFileBytes(Bytes::from(t)),
             Some(AddTorrentOptions {
                 overwrite: true,
-                initial_peers: Some(vec![OTHER_CLIENT_ADDR]),
+                initial_peers: Some(vec![addr]),
                 ..Default::default()
             }),
         )
