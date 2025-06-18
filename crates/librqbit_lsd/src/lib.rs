db00d8aacf5f33e77335f670bdf2cd4b2a087e46
Igor Katson <igor.katson@gmail.com>
2025-06-18 15:32:26 +0100

    Rewrite lsd a bit to make it procedural

--- crates/librqbit_lsd/src/lib.rs
+++ crates/librqbit_lsd/src/lib.rs
@@ -1,6 +1,7 @@
 use std::{
     collections::HashMap,
     net::{Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrV4, SocketAddrV6},
+    num::NonZero,
     str::{FromStr, from_utf8},
     sync::Arc,
     time::Duration,
@@ -8,6 +9,7 @@ use std::{
 
 use anyhow::Context;
 use futures::Stream;
+use governor::{DefaultDirectRateLimiter as RateLimiter, Quota};
 use librqbit_core::{Id20, spawn_utils::spawn_with_cancel};
 use librqbit_dualstack_sockets::MulticastUdpSocket;
 use parking_lot::RwLock;
@@ -27,6 +29,8 @@ const LSD_IPV6: SocketAddrV6 = SocketAddrV6::new(
 struct Announce {
     tx: UnboundedSender<SocketAddr>,
     port: Option<u16>,
+    reply_ratelimit_ipv4: RateLimiter,
+    reply_ratelimit_ipv6: RateLimiter,
 }
 
 struct LocalServiceDiscoveryInner {
@@ -96,56 +100,89 @@ cookie: {cookie}\r
         )
     }
 
+    async fn recv_and_process_one(&self, buf: &mut [u8]) -> anyhow::Result<()> {
+        macro_rules! return_if_none {
+            ($e:expr) => {
+                return_if_none!($e, ())
+            };
+            ($e:expr, $if_err:expr) => {
+                match $e {
+                    Some(e) => e,
+                    None => {
+                        $if_err;
+                        return Ok(());
+                    }
+                }
+            };
+        }
+
+        let mut headers = [httparse::EMPTY_HEADER; 16];
+
+        let (sz, addr) = self.inner.socket.recv_from(buf).await?;
+        let buf = bstr::BStr::new(&buf[..sz]);
+
+        let bts = return_if_none!(
+            try_parse_bt_search(buf, &mut headers)
+                .inspect_err(|e| trace!(?buf, ?addr, "error parsing message: {e:#}"))
+                .ok()
+        );
+
+        trace!(?addr, ?bts, "received");
+
+        if bts.our_cookie == Some(self.inner.cookie) {
+            trace!(?bts, "ignoring our own message");
+            return Ok(());
+        }
+
+        let announce_port = {
+            let g = self.inner.receivers.read();
+            let announce = return_if_none!(g.get(&bts.hash));
+            let mut addr = addr;
+            addr.set_port(bts.port);
+
+            return_if_none!(announce.tx.send(addr).ok());
+
+            let announce_port = return_if_none!(announce.port);
+
+            let rl = if addr.is_ipv4() {
+                &announce.reply_ratelimit_ipv4
+            } else {
+                &announce.reply_ratelimit_ipv6
+            };
+
+            return_if_none!(
+                rl.check().ok(),
+                trace!(?addr, ?bts, "replying rate-limited")
+            );
+
+            announce_port
+        };
+
+        let mopts = return_if_none!(
+            self.inner.socket.find_mcast_opts_for_replying_to(&addr),
+            debug!(?addr, "couldn't find where to reply")
+        );
+
+        let reply = self.gen_announce_msg(bts.hash, announce_port, addr.is_ipv6());
+
+        if let Err(e) = self
+            .inner
+            .socket
+            .send_multicast_msg(reply.as_bytes(), &mopts)
+            .await
+        {
+            trace!(?addr, ?reply, ?mopts, "error sending reply: {e:#}");
+        } else {
+            trace!(?addr, ?reply, ?mopts, "sent reply");
+        }
+        Ok(())
+    }
+
     async fn task_monitor_recv(self) -> anyhow::Result<()> {
         let mut buf = [0u8; 4096];
 
         loop {
-            let mut headers = [httparse::EMPTY_HEADER; 16];
-            let (sz, addr) = self.inner.socket.recv_from(&mut buf).await?;
-            let buf = bstr::BStr::new(&buf[..sz]);
-            match try_parse_bt_search(buf, &mut headers) {
-                Ok(bts) => {
-                    trace!(?addr, ?bts, "received");
-                    if bts.our_cookie == Some(self.inner.cookie) {
-                        trace!(?bts, "ignoring our own message");
-                        continue;
-                    }
-
-                    let reply_port =
-                        self.inner
-                            .receivers
-                            .read()
-                            .get(&bts.hash)
-                            .and_then(|announce| {
-                                let mut addr = addr;
-                                addr.set_port(bts.port);
-                                announce.tx.send(addr).ok().and(announce.port)
-                            });
-
-                    if let Some(port) = reply_port {
-                        if let Some(mopts) =
-                            self.inner.socket.find_mcast_opts_for_replying_to(&addr)
-                        {
-                            let reply = self.gen_announce_msg(bts.hash, port, addr.is_ipv6());
-                            if let Err(e) = self
-                                .inner
-                                .socket
-                                .send_multicast_msg(reply.as_bytes(), &mopts)
-                                .await
-                            {
-                                trace!(?addr, ?reply, ?mopts, "error sending reply: {e:#}");
-                            } else {
-                                trace!(?addr, ?reply, ?mopts, "sent reply");
-                            }
-                        } else {
-                            debug!(?addr, "couldn't find where to reply");
-                        }
-                    }
-                }
-                Err(e) => {
-                    trace!(?buf, ?addr, "error parsing message: {e:#}");
-                }
-            }
+            self.recv_and_process_one(&mut buf).await?;
         }
     }
 
@@ -196,11 +233,15 @@ cookie: {cookie}\r
             }
         }
 
+        let rl = || RateLimiter::direct(Quota::per_second(NonZero::new(1).unwrap()));
+
         self.inner.receivers.write().insert(
             info_hash,
             Announce {
                 tx,
                 port: announce_port,
+                reply_ratelimit_ipv4: rl(),
+                reply_ratelimit_ipv6: rl(),
             },
         );
 
