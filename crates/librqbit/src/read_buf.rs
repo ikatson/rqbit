016035c691058cd8fbefa955ada8d185f024ee6a
Igor Katson <igor.katson@gmail.com>
2025-06-19 19:46:16 +0100

    Add tests and fix bugs

--- crates/librqbit/src/read_buf.rs
+++ crates/librqbit/src/read_buf.rs
@@ -23,12 +23,19 @@ macro_rules! advance {
     };
 }
 
+macro_rules! as_slice_ranges {
+    ($self:expr) => {{
+        let first_len = $self.len.min(crate::read_buf::BUFLEN - $self.start);
+        let first = $self.start..$self.start + first_len;
+        let second = 0..$self.len.saturating_sub(first_len);
+        (first, second)
+    }};
+}
+
 macro_rules! as_slices {
     ($self:expr) => {{
-        let first_len = $self.len.min(BUFLEN - $self.start);
-        let first = &$self.buf[$self.start..$self.start + first_len];
-        let second = &$self.buf[..$self.len.saturating_sub(first_len)];
-        (first, second)
+        let (first, second) = as_slice_ranges!($self);
+        (&$self.buf[first], &$self.buf[second])
     }};
 }
 
@@ -61,17 +68,36 @@ impl ReadBuf {
     }
 
     fn make_contiguous(&mut self) {
-        todo!()
+        let mut new = [0u8; BUFLEN];
+        let (first, second) = as_slices!(self);
+        new[..first.len()].copy_from_slice(first);
+        new[first.len()..first.len() + second.len()].copy_from_slice(second);
+        self.buf = new;
+        self.start = 0;
     }
 
-    fn write_buf_for_read(&mut self) -> &mut [u8] {
+    #[cfg(test)]
+    fn advance(&mut self, len: usize) {
+        advance!(self, len);
+    }
+
+    fn write_buf_for_read_range(&self) -> std::ops::Range<usize> {
+        if self.len == BUFLEN {
+            return 0..0;
+        }
         let start = (self.start + self.len) % BUFLEN;
+        // TODO: can this be written without if?
         let end = if start < self.start {
             self.start
         } else {
             BUFLEN
         };
-        &mut self.buf[start..end]
+        start..end
+    }
+
+    fn write_buf_for_read(&mut self) -> &mut [u8] {
+        let range = self.write_buf_for_read_range();
+        &mut self.buf[range]
     }
 
     // Read a message into the buffer, try to deserialize it and call the callback on it.
@@ -110,5 +136,75 @@ impl ReadBuf {
                 need_additional_bytes = need_additional_bytes.saturating_sub(size)
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{BUFLEN, ReadBuf};
+
+    #[test]
+    fn test_ringbuf_ranges() {
+        let mut b = ReadBuf::new();
+        assert_eq!(as_slice_ranges!(b), (0..0, 0..0));
+
+        b.start = 10;
+        b.len = 10;
+        assert_eq!(as_slice_ranges!(b), (10..20, 0..0));
+
+        b.start = BUFLEN - 100;
+        b.len = 100;
+        assert_eq!(as_slice_ranges!(b), (BUFLEN - 100..BUFLEN, 0..0));
+
+        b.start = BUFLEN - 100;
+        b.len = 120;
+        assert_eq!(as_slice_ranges!(b), (BUFLEN - 100..BUFLEN, 0..20));
+
+        b.start = BUFLEN - 100;
+        b.len = BUFLEN;
+        assert_eq!(as_slice_ranges!(b), (BUFLEN - 100..BUFLEN, 0..BUFLEN - 100));
+    }
+
+    #[test]
+    fn test_ringbuf_write_range() {
+        let mut b = ReadBuf::new();
+        assert_eq!(b.write_buf_for_read_range(), 0..BUFLEN);
+
+        b.start = 10;
+        b.len = 10;
+        assert_eq!(b.write_buf_for_read_range(), 20..BUFLEN);
+
+        b.start = BUFLEN - 100;
+        b.len = 100;
+        assert_eq!(b.write_buf_for_read_range(), 0..BUFLEN - 100);
+
+        b.start = BUFLEN - 100;
+        b.len = 120;
+        assert_eq!(b.write_buf_for_read_range(), 20..BUFLEN - 100);
+
+        b.start = BUFLEN - 100;
+        b.len = BUFLEN - 1;
+        assert_eq!(b.write_buf_for_read_range(), BUFLEN - 101..BUFLEN - 100);
+
+        b.start = BUFLEN - 100;
+        b.len = BUFLEN;
+        assert_eq!(b.write_buf_for_read_range().len(), 0);
+    }
+
+    #[test]
+    fn test_ringbuf_advance() {
+        let mut b = ReadBuf::new();
+
+        b.start = 10;
+        b.len = 10;
+        assert_eq!(as_slice_ranges!(b), (10..20, 0..0));
+        b.advance(5);
+        assert_eq!(as_slice_ranges!(b), (15..20, 0..0));
+
+        b.start = BUFLEN - 5;
+        b.len = 10;
+        assert_eq!(as_slice_ranges!(b), (BUFLEN - 5..BUFLEN, 0..5));
+        b.advance(5);
+        assert_eq!(as_slice_ranges!(b), (0..5, 0..0));
     }
 }
