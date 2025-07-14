d54ef584258afa11719b68504d34cc6d43390138
Igor Katson <igor.katson@gmail.com>
2025-07-14 17:54:04 +0100

    Reducing re-renders

--- crates/librqbit/webui/src/components/CompactTorrentRow.tsx
+++ crates/librqbit/webui/src/components/CompactTorrentRow.tsx
@@ -1,9 +1,9 @@
+import { useMemo } from "react";
 import { TorrentIdWithStats, STATE_INITIALIZING } from "../api-types";
 import { formatBytes } from "../helper/formatBytes";
 import { getCompletionETA } from "../helper/getCompletionETA";
 import { useTorrentStore } from "../stores/torrentStore";
 import { ProgressBar } from "./ProgressBar";
-import { Speed } from "./Speed";
 import { StatusIcon } from "./StatusIcon";
 
 export const CompactTorrentRow: React.FC<{
@@ -29,17 +29,6 @@ export const CompactTorrentRow: React.FC<{
       ? 100
       : (progressBytes / totalBytes) * 100;
 
-  const statusIcon = (className: string) => {
-    return (
-      <StatusIcon
-        className={className}
-        error={!!error}
-        live={!!statsResponse?.live}
-        finished={finished}
-      />
-    );
-  };
-
   const formatPeersString = () => {
     let peer_stats = statsResponse?.live?.snapshot.peer_stats;
     if (!peer_stats) {
@@ -48,21 +37,32 @@ export const CompactTorrentRow: React.FC<{
     return `${peer_stats.live} / ${peer_stats.seen}`;
   };
 
+  const onClick = useMemo(() => {
+    return (event: React.MouseEvent<HTMLTableRowElement>) => {
+      event.stopPropagation();
+      event.preventDefault();
+      if (event.metaKey || event.ctrlKey || event.shiftKey) {
+        toggleSelectedTorrentId(torrent.id);
+      } else {
+        setSelectedTorrentId(torrent.id);
+      }
+    };
+  }, [torrent.id]);
+
   return (
     <tr
       className={`cursor-pointer ${selected ? "bg-gray-200 dark:bg-slate-700" : ""}`}
-      onClick={(event) => {
-        event.stopPropagation();
-        event.preventDefault();
-        if (event.metaKey || event.ctrlKey || event.shiftKey) {
-          toggleSelectedTorrentId(torrent.id);
-        } else {
-          setSelectedTorrentId(torrent.id);
-        }
-      }}
+      onClick={onClick}
     >
       <td className="px-2 py-1 whitespace-nowrap text-xs">{torrent.id}</td>
-      <td className="px-2 py-1 whitespace-nowrap">{statusIcon("w-4 h-4")}</td>
+      <td className="px-2 py-1 whitespace-nowrap">
+        <StatusIcon
+          className="w-4 h-4"
+          error={!!error}
+          live={!!statsResponse?.live}
+          finished={finished}
+        />
+      </td>
       <td className="px-2 py-1 text-left text-xs text-gray-900 text-ellipsis overflow-hidden dark:text-slate-200">
         {torrent.name}
       </td>
--- crates/librqbit/webui/src/components/TorrentDetailsPane.tsx
+++ crates/librqbit/webui/src/components/TorrentDetailsPane.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useMemo, useState } from "react";
 import { TorrentDetails, TorrentIdWithStats } from "../api-types";
 import { FileListInput } from "./FileListInput";
 import { TorrentActions } from "./TorrentActions";
@@ -12,42 +12,49 @@ import { TorrentActionsMulti } from "./TorrentActionsMulti";
 
 const noop = () => {};
 
-export const TorrentDetailsPane: React.FC<{}> = () => {
-  const selectedTorrents = useTorrentStore((state) =>
-    state.selectedTorrentIds
-      .map((id) => (state.torrents || []).find((t) => t.id === id))
-      .filter((t) => t !== undefined),
+export const TorrentDetailsPaneSingleSelected: React.FC<{ id: number }> = ({
+  id,
+}) => {
+  const torrent = useTorrentStore((state) =>
+    state.torrents?.find((t) => t.id === id),
   );
-  if (selectedTorrents.length === 0) {
-    return null;
-  } else if (selectedTorrents.length === 1) {
-    const torrent = selectedTorrents[0];
-    return (
-      <div>
-        <div className="p-2 bg-gray-100 dark:bg-gray-800">
-          <TorrentActions
-            torrent={torrent}
-            extendedView={false}
-            setExtendedView={noop}
-          />
-        </div>
-        <Tabs tabs={["Overview", "Files", "Peers"]}>
-          <Tab>
-            <TorrentDetailsOverviewTab torrent={torrent} />
-          </Tab>
-          <Tab>
-            <ManagedTorrentFileListInput torrent={torrent} />
-          </Tab>
-          <Tab>
-            <PeerTable torrent={torrent} />
-          </Tab>
-        </Tabs>
+  if (!torrent) return null;
+  return (
+    <div>
+      <div className="p-2 bg-gray-100 dark:bg-gray-800">
+        <TorrentActions
+          torrent={torrent}
+          extendedView={false}
+          setExtendedView={noop}
+        />
       </div>
-    );
+      <Tabs tabs={["Overview", "Files", "Peers"]}>
+        <Tab>
+          <TorrentDetailsOverviewTab torrent={torrent} />
+        </Tab>
+        <Tab>
+          <ManagedTorrentFileListInput torrent={torrent} />
+        </Tab>
+        <Tab>
+          <PeerTable torrent={torrent} />
+        </Tab>
+      </Tabs>
+    </div>
+  );
+};
+
+export const TorrentDetailsPane: React.FC<{}> = () => {
+  const selectedTorrentIds = useTorrentStore(
+    (state) => state.selectedTorrentIds,
+  );
+  if (selectedTorrentIds.length === 0) {
+    return null;
+  } else if (selectedTorrentIds.length === 1) {
+    return <TorrentDetailsPaneSingleSelected id={selectedTorrentIds[0]} />;
   } else {
     return (
       <div className="p-2 bg-gray-100 dark:bg-gray-800">
-        <TorrentActionsMulti torrents={selectedTorrents} />
+        <TorrentActionsMulti torrentIds={selectedTorrentIds} />
       </div>
     );
   }
