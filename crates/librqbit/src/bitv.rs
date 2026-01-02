a5286d6c37ca518c74d02ce785e82c6f67afd789
Igor Katson <igor.katson@gmail.com>
2026-01-02 07:59:01 +0000

    Remove impl BitV for BitVec

--- crates/librqbit/src/bitv.rs
+++ crates/librqbit/src/bitv.rs
@@ -5,7 +5,6 @@ use bitvec::{
     boxed::BitBox,
     order::Msb0,
     slice::BitSlice,
-    vec::BitVec,
     view::{AsBits, AsMutBits},
 };
 use tracing::trace;
@@ -36,29 +35,6 @@ impl MmapBitV {
         let mmap =
             unsafe { memmap2::MmapOptions::new().map_mut(&file) }.context("error mmapping file")?;
         Ok(Self { mmap, _file: file })
-    }
-}
-
-#[async_trait::async_trait]
-impl BitV for BitVec<u8, Msb0> {
-    fn as_slice(&self) -> &BitSlice<u8, Msb0> {
-        self.as_bitslice()
-    }
-
-    fn as_slice_mut(&mut self) -> &mut BitSlice<u8, Msb0> {
-        self.as_mut_bitslice()
-    }
-
-    fn as_bytes(&self) -> &[u8] {
-        self.as_raw_slice()
-    }
-
-    fn flush(&mut self, _flush_async: bool) -> anyhow::Result<()> {
-        Ok(())
-    }
-
-    fn into_dyn(self) -> Box<dyn BitV> {
-        Box::new(self)
     }
 }
 
