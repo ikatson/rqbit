f7d162964898776cc2286b7e1eee29aad5130d03
Alexander Keith <alex.michael.keith@gmail.com>
2026-01-12 17:59:27 -0500

    feat: make peer limit configurable per torrent

--- crates/librqbit/src/session.rs
+++ crates/librqbit/src/session.rs
@@ -150,6 +150,7 @@ pub struct Session {
     // Feature flags
     #[cfg(feature = "disable-upload")]
     _disable_upload: bool,
+    pub peer_limit: Option<usize>,
 }
 
 async fn torrent_from_url(
@@ -278,6 +279,9 @@ pub struct AddTorrentOptions {
 
     /// Initial peers to start of with.
     pub initial_peers: Option<Vec<SocketAddr>>,
+
+    /// Max concurrent connected peers.
+    pub peer_limit: Option<usize>,
 
     /// This is used to restore the session from serialized state.
     pub preferred_id: Option<usize>,
@@ -450,6 +454,9 @@ pub struct SessionOptions {
 
     // The list of tracker URLs to always use for each torrent.
     pub trackers: HashSet<url::Url>,
+
+    /// Default peer limit per torrent.
+    pub peer_limit: Option<usize>,
 
     #[cfg(feature = "disable-upload")]
     pub disable_upload: bool,
@@ -745,6 +752,7 @@ impl Session {
                 ratelimits: Limits::new(opts.ratelimits),
                 trackers: opts.trackers,
                 disable_trackers: opts.disable_trackers,
+                peer_limit: opts.peer_limit,
 
                 #[cfg(feature = "disable-upload")]
                 _disable_upload: opts.disable_upload,
@@ -1293,6 +1301,7 @@ impl Session {
                     disk_write_queue: self.disk_write_tx.clone(),
                     ratelimits: opts.ratelimits,
                     initial_peers: opts.initial_peers.clone().unwrap_or_default(),
+                    peer_limit: opts.peer_limit.or(self.peer_limit),
                     #[cfg(feature = "disable-upload")]
                     _disable_upload: self._disable_upload,
                 },
