c4f4d83e8d55d444d8e8d101bd547bd18a32db26
Igor Katson <igor.katson@gmail.com>
2026-01-12 12:38:28 +0000

    Update webui deps

--- crates/librqbit/webui/vite.config.ts
+++ crates/librqbit/webui/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), tailwindcss()],
   server: {
     host: true,
     port: 3031,
