c343d24806b844207fb8ebf00a2e67665b4116b6
Vincent Cheung <vincentcheung@gmail.com>
2026-08-24 16:37:00 -0700

    feat(streaming): make lookahead configurable per stream

--- crates/librqbit/src/lib.rs
+++ crates/librqbit/src/lib.rs
@@ -97,8 +97,8 @@ pub use session::{
 };
 pub use stream_connect::ConnectionOptions;
 pub use torrent_state::{
-    ManagedTorrent, ManagedTorrentShared, ManagedTorrentState, TorrentMetadata, TorrentStats,
-    TorrentStatsState,
+    FileStreamOptions, ManagedTorrent, ManagedTorrentShared, ManagedTorrentState, TorrentMetadata,
+    TorrentStats, TorrentStatsState,
 };
 pub use type_aliases::FileInfos;
 
